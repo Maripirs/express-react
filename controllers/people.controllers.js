90414874cb1e446f2d7c9ffbe77a988f5c9a26b3
Maripirs <mariapazmaluenda@gmail.com>
2022-12-21 14:13:50 -0800

    added update and delete

--- controllers/people.controllers.js
+++ controllers/people.controllers.js
@@ -23,16 +23,29 @@ router.post("/", async (req, res, next) => {
 });
 
 router.get("/:id", async (req, res) => {
-	res.status(200).json({ message: "people show route: " + req.params.id });
+	try {
+		res.json(await People.findById(req.params.id));
+	} catch (error) {
+		res.status(400).json(error);
+	}
 });
 
 router.delete("/:id", async (req, res) => {
-	res.status(200).json({ message: "people delete route: " + req.params.id });
+	try {
+		res.json(await People.findByIdAndDelete(req.params.id));
+	} catch (error) {
+		res.status(400).json(error);
+	}
 });
 
 router.put("/:id", async (req, res) => {
-	console.log(req.body);
-	res.status(200).json({ message: "people update route: " + req.params.id });
+	try {
+		res.json(await People.findByIdAndUpdate(req.params.id), req.body, {
+			new: true,
+		});
+	} catch (error) {
+		res.status(400).json(error);
+	}
 });
 
 module.exports = router;
